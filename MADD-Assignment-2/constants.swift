9823a4ab78d766aba6884950e98e5f6f1bcb9980
SE <se@iMac-02.local>
2018-09-30 21:08:45 +0530

    added functions to return url

--- MADD-Assignment-2/constants.swift
+++ MADD-Assignment-2/constants.swift
@@ -3,7 +3,18 @@
 //  MADD-Assignment-2
 //
 //  Created by SE on 9/30/18.
-//  Copyright © 2018 Nandun Bandara. All rights reserved.
+//  Copyright © 2018 Isuru Prasad. All rights reserved.
 //
 
 import Foundation
+
+class Constants {
+    
+    func getSearchApiUrl (term:String, limit:Int, entity:String) -> String{
+        
+        var url = "https://itunes.apple.com/search?term=" + term +
+            "&limit=" + limit + "&entity=" + entity;
+        
+        return url;
+    }
+}
--- MADD-Assignment-2/http.service.swift
+++ MADD-Assignment-2/http.service.swift
@@ -3,7 +3,17 @@
 //  MADD-Assignment-2
 //
 //  Created by SE on 9/30/18.
-//  Copyright © 2018 Nandun Bandara. All rights reserved.
+//  Copyright © 2018 Isuru Prasad. All rights reserved.
 //
 
 import Foundation
+
+
+func getdata(url: String){
+
+    let term = "puzzle";
+    let limit = 200;
+    let entity = "software";
+    
+    guard let url = URL(string: Constants.getSearchApiUrl(term,limit,entity))
+}
