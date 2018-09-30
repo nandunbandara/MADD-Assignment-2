b1b32fd032ddefec940c5babd48de9c132084d22
navoda-brightsparks <it15110244@my.sliit.lk>
2018-09-30 19:16:29 +0530

    Initial Commit

---
+++ MADD-Assignment-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MADD-Assignment-2
+//
+//  Created by SE on 9/30/18.
+//  Copyright © 2018 Nandun Bandara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
