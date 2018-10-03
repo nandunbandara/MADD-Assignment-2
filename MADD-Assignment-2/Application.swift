2267b36ff27cf2f14e94f210c6c3aeab6fd81007
prasadnwi <prasadnwip@gmail.com>
2018-10-03 21:44:15 +0530

    added application object

--- MADD-Assignment-2/Application.swift
+++ MADD-Assignment-2/Application.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2018 Nandun Bandara. All rights reserved.
 //
 
-import Foundation
+class Application {
+    
+    var name: String?
+    var imageUrl: String?
+    var description: String?
+    var features: [String]?
+    var price: Float?
+    var avgRating: Float?
+    
+    init(_ params: [String: Any]) {
+        
+        self.name = params["trackName"] as? String
+        
+    }
+    
+}
+
