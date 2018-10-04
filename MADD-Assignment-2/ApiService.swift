804dc34b86a265abaa19415d4a5dfeb75bc59b87
prasadnwi <prasadnwip@gmail.com>
2018-10-04 08:02:20 +0530

    Dependencies added. Implemented .gitignore

---
+++ MADD-Assignment-2/ApiService.swift
@@ -0,0 +1,49 @@
+//
+//  ApiService.swift
+//  MADD-Assignment-2
+//
+//  Created by Isuru Prasad on 10/3/18.
+//  Copyright © 2018 Nandun Bandara. All rights reserved.
+//
+
+
+import Alamofire
+
+class API {
+    
+    static func fetchApplications(_ term: String, limit: Int, entity: String, completion: @escaping ([Application]?) -> Void) {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=puzzle&limit=20&entity=software") else {
+            completion(nil)
+            return
+        }
+        Alamofire.request(url,
+                          method: .get,
+                          parameters: ["include_docs": "true"])
+            .validate()
+            .responseJSON { response in
+                guard response.result.isSuccess else {
+                    print("Error while fetching remote rooms: \(String(describing: response.result.error))")
+                    completion(nil)
+                    return
+                }
+                
+                guard let value = response.result.value as? [String: Any] else {
+                    completion(nil)
+                    return
+                }
+                
+                guard let results = value["results"] as? [[String: Any]] else {
+                    completion(nil)
+                    return
+                }
+                
+                var applications: [Application] = []
+                for app in results {
+                    let application = Application(app)
+                    applications.append(application)
+                }
+                
+                completion(applications)
+        }
+    }
+}
--- MADD-Assignment-2/Application.swift
+++ MADD-Assignment-2/Application.swift
@@ -2,7 +2,7 @@
 //  Application.swift
 //  MADD-Assignment-2
 //
-//  Created by Nandun Bandara on 10/3/18.
+//  Created by Isuru Prasad on 10/3/18.
 //  Copyright © 2018 Nandun Bandara. All rights reserved.
 //
 
